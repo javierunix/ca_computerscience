c95f86c8c34b798edaa866f4a97a4c0322df618a
Javier Guijarro <fjguijarromartinez@gmail.com>
2021-05-18 19:52:41 +0200

    finish setting up

---
+++ CS101/control_flow/magic_8_ball.py
@@ -0,0 +1,32 @@
+"""
+The Magic 8-Ball is a popular toy 
+developed in the 1950s for fortune-telling or advice seeking.
+Write a magic8.py Python program that can answer any “Yes” or “No” question 
+with a different fortune each time it executes.
+
+We’ll be using the following 9 possible answers for our Magic 8-Ball
+
+"""
+#               ******* Setting up *********
+
+
+# Step 1 
+
+# In magic8.py, declare a variable name and assign it to the name of the person
+# who will be asking the Magic 8-Ball.
+
+name = "Noa"
+
+#Step 2
+
+# Next, declare a variable question, and assign to a “Yes” or “No” 
+# question you’d like to ask the Magic 8-Ball.
+
+question = "Do you like napolitan pizza?"
+
+# Step 3
+#We want to store the answer of the Magic 8-Ball in another variable, 
+# which we’ll call answer. 
+#For now, assign this variable to an empty string.
+
+answer = ""
