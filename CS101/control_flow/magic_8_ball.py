3ffc5049ddee2f582623d06bae9405789944485c
Javier Guijarro <fjguijarromartinez@gmail.com>
2021-05-18 20:01:54 +0200

    correct typo

--- CS101/control_flow/magic_8_ball.py
+++ CS101/control_flow/magic_8_ball.py
@@ -5,28 +5,22 @@ Write a magic8.py Python program that can answer any “Yes” or “No” quest
 with a different fortune each time it executes.
 
 We’ll be using the following 9 possible answers for our Magic 8-Ball
-
 """
+
 #               ******* Setting up *********
 
-
 # Step 1 
-
-# In magic8.py, declare a variable name and assign it to the name of the person
+# In magic_8_ball.py, declare a variable name and assign it to the name of the person
 # who will be asking the Magic 8-Ball.
-
 name = "Noa"
 
 #Step 2
-
 # Next, declare a variable question, and assign to a “Yes” or “No” 
 # question you’d like to ask the Magic 8-Ball.
-
 question = "Do you like napolitan pizza?"
 
 # Step 3
 #We want to store the answer of the Magic 8-Ball in another variable, 
 # which we’ll call answer. 
 #For now, assign this variable to an empty string.
-
 answer = ""
