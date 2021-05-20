d93a55ff4a18f2718107da0473346a30e2c9333a
Javier Guijarro <fjguijarromartinez@gmail.com>
2021-05-20 23:35:42 +0200

    finish advanced challenges

--- CS101/functions/challenges_advanced.py
+++ CS101/functions/challenges_advanced.py
@@ -35,3 +35,27 @@ def max_num(nums):
     return maximus
 print(max_num([50, -10, 0, 75, 20]))    
 
+# Write a function named same_values() that takes two lists of numbers of equal size as parameters.
+# The function should return a list of the indices where the values were equal in lst1 and lst2.
+# For example, the following code should return [0, 2, 3]:
+# same_values([5, 1, -10, 3, 3], [5, 10, -10, 3, 5])
+
+def same_values(nums1, nums2):
+    index_list = []
+    for i in range(len(nums1)):
+        if nums1[i] == nums2[i]:
+            index_list.append(i)
+    return index_list
+print(same_values([5, 1, -10, 3, 3], [5, 10, -10, 3, 5]))
+
+# Create a function named reversed_list() that takes two lists of the same size as parameters named lst1 and lst2.
+# The function should return True if lst1 is the same as lst2 reversed. The function should return False otherwise.
+# For example, reversed_list([1, 2, 3], [3, 2, 1]) should return True.
+
+def reversed_list(lst1, lst2):
+    reversed = True # set this variable as true as default
+    for i in range(len(lst1)):
+        if lst1[i] != lst2[-( i+ 1)]: # if a element is different its symmetrical
+            reversed = False # ... then the list are no reversed
+    return reversed
+print(reversed_list([1, 2, 3], [3, 2, 1]))
