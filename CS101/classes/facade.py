ab0b20deedf57a3215ddc03f5539a0241a7ed168
Francisco Javier Guijarro <fjguijarromartinez@gmail.com>
2021-05-21 22:21:06 +0200

    add classes tutorial

---
+++ CS101/classes/facade.py
@@ -0,0 +1,10 @@
+# Observatios:
+# A namespace is a collection of defined symbolic names along with information
+# about the object that the name references.
+
+class Facade: # Define a class with the 'class' keyword. It is recommended capitalize the class name
+	pass
+
+# Make a Facade instance and save it to the variable facade_1
+
+facade_1 = Facade()
