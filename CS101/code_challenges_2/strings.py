5c73587fcaf010ac8cb16b3426bdfaede9cb8940
Francisco Javier Guijarro <fjguijarromartinez@gmail.com>
2021-05-24 13:12:08 +0200

    finish string challenges

--- CS101/code_challenges_2/strings.py
+++ CS101/code_challenges_2/strings.py
@@ -78,6 +78,78 @@ def x_length_words(sentence, x):
 
 	return True # otherwise return true
 
-print(x_length_words("i like apples", 2))
-print(x_length_words("he likes apples", 2))
+# print(x_length_words("i like apples", 2))
+# print(x_length_words("he likes apples", 2))
+
+# Write a function called check_for_name that takes two strings as parameters named sentence and name. 
+
+# The function should return True if name appears in sentence in all lowercase letters, 
+# all uppercase letters, or with any mix of uppercase and lowercase letters. The function should return False otherwise.
+
+# ********************** 6 *****************************
+
+def check_for_name(sentence, name):
+	name_lower = name.lower() # transform name in lowercase
+	sentece_lower = sentence.lower() # transform sentence in lowercase
+	return(name_lower in sentece_lower) # check if name is in sentence
+
+# print(check_for_name("My name is jamie", "Jamie"))
+# print(check_for_name("My name is Samantha", "Jamie"))
+
+# ********************** 7 *****************************
+
+# Create a function named every_other_letter that takes a string named word as a parameter. 
+# The function should return a string containing every other letter in word.
+
+def every_other_letter(word):
+	new_string = '' # create a new string 
+	for i in range(0, len(word), 2): # loop every two letters
+		new_string += word[i] # add letter to new string
+	return new_string
+
+
+# print(every_other_letter("Codecademy"))
+# print(every_other_letter("Hello world!"))
+# print(every_other_letter(""))
+
+# ********************** 7 *****************************
+
+# Write a function named reverse_string that has a string named word as a parameter. 
+# The function should return word in reverse.
+
+def reverse_string(word):
+	reverse = ""
+	for i in range(len(word)):
+		reverse = word[i] + reverse # we add the new letter before the current string
+	return reverse
+
+# print(reverse_string("Codecademy"))
+
+
+# ********************** 8 *****************************
+
+# Write a function called make_spoonerism that takes two strings as parameters named word1 and word2. 
+# Finding the first syllable of a word is a difficult task, so for our function, we’re going to switch the first letters of each word. 
+# Return the two new words as a single string separated by a space.
+
+def make_spoonerism(word1, word2):
+	return word2[0]+word1[1:] + " " + word1[0]+word2[1:]
+
+# print(make_spoonerism("Codecademy", "Learn"))
+# print(make_spoonerism("Hello", "world!"))
+# print(make_spoonerism("a", "b"))
+
+
+# ********************** 9 *****************************
+
+# Create a function named add_exclamation that has one parameter named word. 
+# This function should add exclamation points to the end of word until word is 20 characters long. 
+# If word is already at least 20 characters long, just return word.
+
+def add_exclamation(word):
+	while len(word) < 20:
+		word += "!"
+	return word
+print(add_exclamation("Codecademy"))
+print(add_exclamation("Codecademy is the best place to learn"))
 
