d3208c53ec792b8493ccfe68a3514d70631aa02e
Francisco Javier Guijarro <fjguijarromartinez@gmail.com>
2021-06-09 18:52:52 +0200

    done exercise 4

--- CS102/Recursions/recursion_vs_iterations.py
+++ CS102/Recursions/recursion_vs_iterations.py
@@ -50,3 +50,77 @@ def fibonacci_iterative(n):
     n_1 = result
 
   return result
+
+# 5. *********************** iterative implementation of the sum digits function **************************************
+
+# Linear - O(N), where "N" is the number of digits in the number
+def sum_digits(n):
+  if n < 0:
+    ValueError("Inputs 0 or greater only!")
+  result = 0
+  while n != 0:
+    result += n % 10
+    n = n // 10
+  return result + n
+ 
+# print(sum_digits(12))
+# # 1 + 2
+# # 3
+# print(sum_digits(552))
+# # 5 + 5 + 2
+# # 12
+# print(sum_digits(123456789))
+# # 1 + 2 + 3 + 4...
+# # 45
+
+# 5. *********************** recursive implementation of the sum digits function **************************************
+
+def sum_digits_recursion(n):
+  
+  if n <= 9: # Base case: if n is a number with a unique digit
+    return n
+  last_digit = n % 10
+  
+  result = sum_digits_recursion(n // 10) + last_digit # recursive function
+  
+  return result
+
+# print(sum_digits_recursion(12))
+# print(sum_digits_recursion(552))
+# print(sum_digits_recursion(123456789))
+
+# 6. *********************** iterative implementation of the minimum function **************************************
+
+def find_min(my_list):
+  min = None
+
+  for element in my_list:
+    if not min or (element < min):
+      min = element
+
+  return min
+
+find_min([42, 17, 2, -1, 67])
+# -1
+#find_mind([])
+# None
+find_min([13, 72, 19, 5, 86])
+# 5
+
+# 7. *********************** recursive implementation of the minimum function **************************************
+
+
+def find_min(my_list, my_min = None):
+  if not my_list:
+    return my_min
+
+  if not my_min or my_list[0] < my_min:
+    my_min = my_list[0]
+  return find_min(my_list[1:], my_min)
+
+# test cases
+print(find_min([42, 17, 2, -1, 67]) == -1)
+print(find_min([]) == None)
+print(find_min([13, 72, 19, 5, 86]) == 5)
+
+
