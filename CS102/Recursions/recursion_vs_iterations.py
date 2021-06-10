1678588aac64015fcf2b95383054ea58faf3e005
Francisco Javier Guijarro <fjguijarromartinez@gmail.com>
2021-06-10 14:11:11 +0200

    finish recursion

--- CS102/Recursions/recursion_vs_iterations.py
+++ CS102/Recursions/recursion_vs_iterations.py
@@ -165,6 +165,41 @@ def multiplication(num1, num2):
   else:
     return num1 + multiplication(num1, (num2 - 1))
 
-print(multiplication(3, 7) == 21)
-print(multiplication(5, 5) == 25)
-print(multiplication(0, 4) == 0)
+# print(multiplication(3, 7) == 21)
+# print(multiplication(5, 5) == 25)
+# print(multiplication(0, 4) == 0)
+
+
+# 11 *********************** recursive implementation of the get the tree depth function **************************************
+
+def build_bst(my_list):
+  if len(my_list) == 0:
+    return None
+
+  mid_idx = len(my_list) // 2
+  mid_val = my_list[mid_idx]
+
+  tree_node = {"data": mid_val}
+  tree_node["left_child"] = build_bst(my_list[ : mid_idx])
+  tree_node["right_child"] = build_bst(my_list[mid_idx + 1 : ])
+
+  return tree_node
+
+
+
+def depth(tree, level = 0):
+  if not isinstance(tree, dict) or not tree:
+    return level
+  return max(depth(tree[key], level + 1) for key in tree)
+
+# HELPER VARIABLES
+tree_level_1 = build_bst([1])
+tree_level_2 = build_bst([1, 2, 3])
+tree_level_4 = build_bst([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]) 
+
+
+print(depth(tree_level_1) == 1)
+print(depth(tree_level_2) == 2)
+print(depth(tree_level_4) == 4)
+
+
