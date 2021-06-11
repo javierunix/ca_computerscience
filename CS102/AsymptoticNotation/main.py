ae9b7ab82eb3b75fe398f500a3b48c38c10b1ee1
Francisco Javier Guijarro <fjguijarromartinez@gmail.com>
2021-06-12 00:31:04 +0200

    exercise 2

--- CS102/AsymptoticNotation/main.py
+++ CS102/AsymptoticNotation/main.py
@@ -1,14 +1,44 @@
-def this_function(number):
-    i = 1
-    while i != number:
-        print(i)
-        i+= 1
-    
-    count = 0
-    while number != 1:
-        count += 1
-        number = number // 2
-    
-    return count
+from linkedlist import LinkedList
 
-print(this_function(8))
+#Fill in Function
+def find_max(linked_list):
+  print("--------------------------")
+  print("Finding the maximum value of:\n{0}".format(linked_list.stringify_list()))
+  #Write Code Here
+  result = linked_list.head_node.get_value()
+
+  while linked_list.head_node.get_next_node():
+    linked_list.head_node = linked_list.head_node.get_next_node() # iterate over the nodes
+    if linked_list.head_node.get_value() >= result:
+      result = linked_list.head_node.get_value()
+  return result
+  
+  
+  
+
+#Test Cases
+ll = LinkedList(6)
+ll.insert_beginning(32)
+ll.insert_beginning(-12)
+ll.insert_beginning(48)
+ll.insert_beginning(2)
+ll.insert_beginning(1)
+print("The maximum value in this linked list is {0}\n".format(find_max(ll)))
+
+ll_2 = LinkedList(60)
+ll_2.insert_beginning(12)
+ll_2.insert_beginning(22)
+ll_2.insert_beginning(-10)
+print("The maximum value in this linked list is {0}\n".format(find_max(ll_2)))
+
+ll_3 = LinkedList("A")
+ll_3.insert_beginning("X")
+ll_3.insert_beginning("V")
+ll_3.insert_beginning("L")
+ll_3.insert_beginning("D")
+ll_3.insert_beginning("Q")
+print("The maximum value in this linked list is {0}\n".format(find_max(ll_3)))
+
+#Runtime
+runtime = "N"
+print("The runtime of find_max is O({0})".format(runtime))
