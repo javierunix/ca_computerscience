a653dc1f5378af39be759b9a144a889266d7e898
Francisco Javier Guijarro <fjguijarromartinez@gmail.com>
2021-06-12 17:30:56 +0200

    finish asymptotic notation

--- CS102/AsymptoticNotation/HashMaps_vs_LinkedLists/script.py
+++ CS102/AsymptoticNotation/HashMaps_vs_LinkedLists/script.py
@@ -21,16 +21,23 @@ my_linked_list.insert_beginning(["Gary", "Bacterial Meningitis"])
 my_linked_list.insert_beginning(["Neeknaz", "Broken Cheekbone"])
 
 #Get Zachary's Disease from a HashMap
-hashmap_zachary_disease = "REPLACE" #Checkpoint 1
+hashmap_zachary_disease = my_hashmap.retrieve("Zachary") #Checkpoint 1
 print("Zachary's disease is {0}".format(hashmap_zachary_disease))
-hashmap_runtime = "REPLACE" #Checkpoint 2
+hashmap_runtime = "1" #Checkpoint 2
 print("The runtime of retrieving a value from a hashmap is O({0})\n\n".format(hashmap_runtime))
 
 
 #Get Zachary's Disease from a Linked List
 #Write Code here for Checkpoint 3
 linked_list_zachary_disease = "REPLACE"
-print("Zachary's disease is {0}".format(linked_list_zachary_disease))
-linked_list_runtime = "REPLACE" #Checkpoint 4
-print("The runtime of retrieving the first value added to a linked list is O({0})\n\n".format(linked_list_runtime))
 
+tranverse = my_linked_list.get_head_node()
+
+while tranverse.get_value()[0] != "Zachary":
+	tranverse = tranverse.get_next_node()
+
+linked_list_zachary_disease = tranverse.get_value()[1]
+
+print("Zachary's disease is {0}".format(linked_list_zachary_disease))
+linked_list_runtime = "N" #Checkpoint 4
+print("The runtime of retrieving the first value added to a linked list is O({0})\n\n".format(linked_list_runtime))
