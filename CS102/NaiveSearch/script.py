6277d1daf71248e43ab3c6e5a1e6ebc53874be2e
Francisco Javier Guijarro <fjguijarromartinez@gmail.com>
2021-06-13 15:10:15 +0200

    update file

--- CS102/NaiveSearch/script.py
+++ CS102/NaiveSearch/script.py
@@ -3,29 +3,39 @@
 # Below the initialized variables, text and pattern, 
 # invoke pattern_search with these two variables passed in as arguments.
 
-def pattern_search(text, pattern):
-    #print("Input Text: %s. Input Patter: %s." %(text, pattern))
+def pattern_search(text, pattern, replacement, case_sensitive = True):
+    fixed_text = ""
+    num_skips = 0
+    counter = 0
+    if case_sensitive == False:
+        text = text.lower()
+        pattern = pattern.lower()
+
     for index in range(len(text)):
-        #print("Text Index: %d" %index)
+        counter += 1
+        if num_skips > 0:
+            num_skips -= 1
+            continue
         match_count = 0
         
         for char in range(len(pattern)):
-            #print("Pattern Index: %d" %char)
-
+            counter += 1
             if text[index + char] == pattern[char]:
-                #print("Matching index found!")
-                #print("Match Count: %d" %match_count)
                 match_count += 1
-
             else:
                 break
         
         if match_count == len(pattern):
-            print(pattern, "found at index: ", index)
+            fixed_text += replacement
+            num_skips = len(pattern) - 1
+        else:
+            fixed_text += text[index]
+    print(counter)
+    return(fixed_text)
 
 
 
 text = "HAYHAYNEEDLEHAYHAYHAYNEEDLEHAYHAYHAYHAYNEEDLE"
-pattern = "NEEDLE"
+pattern = "needle"
 
-pattern_search(text, pattern)
+print(pattern_search(text, pattern, "chains", False))
