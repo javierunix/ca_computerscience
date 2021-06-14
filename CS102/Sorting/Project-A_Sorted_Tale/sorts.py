33bfb61ebcd5616d1c678884ea59ddc5badae3ce
Francisco Javier Guijarro <fjguijarromartinez@gmail.com>
2021-06-14 21:33:55 +0200

    eval condition

--- CS102/Sorting/Project-A_Sorted_Tale/sorts.py
+++ CS102/Sorting/Project-A_Sorted_Tale/sorts.py
@@ -1,12 +1,13 @@
 import random
 
 def bubble_sort(arr):
+  condition = "arr[idx][1] > arr[idx + 1][1]"
   swaps = 0
   sorted = False
   while not sorted:
     sorted = True
     for idx in range(len(arr) - 1):
-      if arr[idx][1] > arr[idx + 1][1]:
+      if eval(condition):
         sorted = False
         arr[idx], arr[idx + 1] = arr[idx + 1], arr[idx]
         swaps += 1
