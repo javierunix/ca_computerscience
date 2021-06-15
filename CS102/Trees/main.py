5e4f9a2cecda0a99bd02acb8d7d0f89ddc8c9533
Francisco Javier Guijarro <fjguijarromartinez@gmail.com>
2021-06-15 21:04:59 +0200

    section 21 finished

--- CS102/Trees/main.py
+++ CS102/Trees/main.py
@@ -1,1 +1,61 @@
-print("Once upon a time...")
+# user_choice = input("What is your name? ")
+# print(user_choice)
+#print("Once upon a time...")
+
+######
+# TREENODE CLASS
+######
+
+# Our TreeNode class will keep track of two things:
+# 1. a portion of the story
+# 2. the choices that the user does in order to progress the story
+class TreeNode:
+
+    def __init__(self, story_piece):
+        self.story_piece = story_piece
+        self.choices = []
+
+    def add_child(self, node):
+        self.choices.append(node)
+
+    def traverse(self):
+        story_node = self
+        print(story_node.story_piece)
+
+
+text_1 = """
+You are in a forest clearing. There is a path to the left.
+A bear emerges from the trees and roars!
+Do you: 
+1 ) Roar back!
+2 ) Run to the left...
+"""
+
+choice_a_text = """
+The bear is startled and runs away.
+Do you:
+1 ) Shout 'Sorry bear!'
+2 ) Yell 'Hooray!'
+"""
+
+
+choice_b_text = """
+You come across a clearing full of flowers. 
+The bear follows you and asks 'what gives?'
+Do you:
+1 ) Gasp 'A talking bear!'
+2 ) Explain that the bear scared you.
+"""
+
+story_root = TreeNode(text_1) # assign to this variable the value text_1
+choice_a = TreeNode(choice_a_text)
+choice_b = TreeNode(choice_b_text)
+
+#print(story_root.story_piece)
+
+story_root.add_child(choice_a)
+story_root.add_child(choice_b)
+#print(story_root.choices)
+
+story_root.traverse()
+
