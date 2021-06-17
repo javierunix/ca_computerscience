e8bf291a6f6586b11a2a919b0f453b14d63e1282
Francisco Javier Guijarro <fjguijarromartinez@gmail.com>
2021-06-17 18:46:40 +0200

    Update newton.py

--- CS102/Sorting/newton.py
+++ CS102/Sorting/newton.py
@@ -4,15 +4,16 @@
 # If the difference between the 2-power of the candidate and the target, half of the search field
 # is rejected and a new candidate is selected.
 
-def newton_sqrt(target_value, allowed_error):
-
-    # at a starting point we assume that the sqrt has to be between 0 and the target value
+def newton_sqrt(target_value, allowed_error): # at a starting point we assume that the sqrt has to be between 0 and the target value
+    count = 0 # variable for counting the number of iterations
     lower_bound = 0 # first lower bound
     upper_bound = target_value # first upper bound
     estimated_sqrt = (lower_bound + upper_bound) / 2 # the estimated sqrt is the middle term
     estimated_power = estimated_sqrt ** 2 
 
     while abs(estimated_power - target_value) > allowed_error: # while the actual error is higher than the allowed
+
+        count += 1
 
         if estimated_power > target_value: # if the power estimated is higher than then actual one
             upper_bound = estimated_sqrt # set the previous estimated sqrt to be the new upper bound
@@ -24,6 +25,6 @@ def newton_sqrt(target_value, allowed_error):
             estimated_sqrt = (lower_bound + upper_bound) / 2
             estimated_power = estimated_sqrt ** 2
 
-    return estimated_sqrt
+    return estimated_sqrt, count
 
-print(newton_sqrt(25000, 0.0001))
+print(newton_sqrt(1000, 0.0001))
