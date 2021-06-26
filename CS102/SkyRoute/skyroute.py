30af607c20cf948520c357cec60e0935d2e77bbb
Francisco Javier Guijarro <fjguijarromartinez@gmail.com>
2021-06-26 15:21:01 +0200

    finish exercise 27

--- CS102/SkyRoute/skyroute.py
+++ CS102/SkyRoute/skyroute.py
@@ -16,6 +16,7 @@ def greet():
 # the function 'skyroute' contains the full program
 def skyroute():
     greet()
+    new_route()
 
 # This function handles the selected origin and destination
 def set_start_and_end(start_point, end_point):
@@ -43,8 +44,48 @@ def set_start_and_end(start_point, end_point):
     return start_point, end_point
 # To request and origin from the user 
 def get_start():
-    pass
+    start_point_letter = input("Where are you coming from? Type in the corresponding letter: ")
+    if start_point_letter in landmark_choices:
+        start_point = landmark_choices[start_point_letter]
+        return start_point
+    else:
+        print("Sorry, that's not a landmark we have data on. Let's try this again...")
+        get_start()
 
 # To request and end from the user 
 def get_end():
-    pass
+    end_point_letter = input("Ok, where are you headed? Type in the corresponding letter: ")
+    if end_point_letter in landmark_choices:
+        end_point= landmark_choices[end_point_letter]
+        return end_point
+    else:
+        print("Sorry, that's not a landmark we have data on. Let's try this again...")
+        get_end()
+
+def new_route(start_point = None, end_point = None):
+    start_point, end_point = set_start_and_end(start_point, end_point)
+
+    shortest_route = get_route(start_point, end_point)
+    shortest_route_string = '\n'.join(shortest_route)
+
+    print("The shortest metro route from {0} to {1} is:\n{2}".format(start_point, end_point, shortest_route_string))
+
+def get_route(start_point, end_point):
+    start_stations = vc_landmarks[start_point]
+    end_stations = vc_landmarks[end_point]
+
+    routes = [] # empty list to collect the shortest routes
+
+    for start_station in start_stations:
+
+        for end_station in end_stations:
+            route = bfs(vc_metro, start_station, end_station)
+
+            if route:
+                routes.append(route)
+
+    shortest_route = min(routes, key=len)
+
+    return shortest_route
+
+skyroute()
